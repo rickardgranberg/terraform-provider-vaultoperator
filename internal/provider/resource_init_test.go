2c74aa86cffa075cac94a4fe2e7c4057f946b9ac
Michael Krupp <m.krupp@chi-deutschland.com>
2021-10-15 19:11:56 +0200

    Cleanup resource code

--- internal/provider/resource_init_test.go
+++ internal/provider/resource_init_test.go
@@ -1,11 +1,23 @@
 package provider
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
+
+var testAccResourceInitVar = fmt.Sprintf("%[1]s.test", resInit)
+var testAccResourceInit = fmt.Sprintf(`
+provider "%[1]s" {
+}
+
+resource "%[2]s" "test" {
+	secret_shares    = 5
+	secret_threshold = 3
+}
+`, provider, resInit)
 
 func TestAccResourceInit(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
@@ -15,29 +27,16 @@ func TestAccResourceInit(t *testing.T) {
 			{
 				Config: testAccResourceInit,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestMatchResourceAttr(
-						"vaultoperator_init.foo", argSecretShares, regexp.MustCompile("5")),
-					resource.TestMatchResourceAttr(
-						"vaultoperator_init.foo", argSecretThreshold, regexp.MustCompile("3")),
-					resource.TestMatchResourceAttr(
-						"vaultoperator_init.foo", argRootToken, regexp.MustCompile(`s\.[A-Za-z0-9]+`)),
-					resource.TestCheckResourceAttrSet("vaultoperator_init.foo", argRootToken),
-					resource.TestMatchResourceAttr("vaultoperator_init.foo", argKeys+".#", regexp.MustCompile("5")),
-					resource.TestMatchResourceAttr("vaultoperator_init.foo", argKeys+".1", regexp.MustCompile("[a-z0-9]+")),
-					resource.TestMatchResourceAttr("vaultoperator_init.foo", argKeysBase64+".#", regexp.MustCompile("5")),
-					resource.TestMatchResourceAttr("vaultoperator_init.foo", argKeysBase64+".1", regexp.MustCompile("[A-Za-z0-9]+")),
+					resource.TestMatchResourceAttr(testAccResourceInitVar, argSecretShares, regexp.MustCompile("5")),
+					resource.TestMatchResourceAttr(testAccResourceInitVar, argSecretThreshold, regexp.MustCompile("3")),
+					resource.TestCheckResourceAttrSet(testAccResourceInitVar, argRootToken),
+					resource.TestMatchResourceAttr(testAccResourceInitVar, argRootToken, regexp.MustCompile(`s\.[A-Za-z0-9]+`)),
+					resource.TestMatchResourceAttr(testAccResourceInitVar, argKeys+".#", regexp.MustCompile("5")),
+					resource.TestMatchResourceAttr(testAccResourceInitVar, argKeys+".1", regexp.MustCompile("[a-z0-9]+")),
+					resource.TestMatchResourceAttr(testAccResourceInitVar, argKeysBase64+".#", regexp.MustCompile("5")),
+					resource.TestMatchResourceAttr(testAccResourceInitVar, argKeysBase64+".1", regexp.MustCompile("[A-Za-z0-9]+")),
 				),
 			},
 		},
 	})
 }
-
-const testAccResourceInit = `
-provider "vaultoperator" {
-}
-
-resource "vaultoperator_init" "foo" {
-	secret_shares    = 5
-	secret_threshold = 3
-}
-`
